ac87f7cca019e175fd67cf57d1e318e139a05610
intelcore <becar_02@mail.ru>
2024-03-10 10:23:37 +0300

    Initial commit

---
+++ src/main/java/com/example/cw20240304/Cw20240304Application.java
@@ -0,0 +1,13 @@
+package com.example.cw20240304;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cw20240304Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cw20240304Application.class, args);
+	}
+
+}
